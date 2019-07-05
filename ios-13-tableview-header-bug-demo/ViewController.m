053cced4e43cd972565f83ee98cccb3366526bee
William Henry <will.henry@seal-one.com>
2019-07-05 15:38:42 +0200

    demonstrate tableview header color working prior to ios13 but failing in ios13 beta

--- ios-13-tableview-header-bug-demo/ViewController.m
+++ ios-13-tableview-header-bug-demo/ViewController.m
@@ -6,9 +6,18 @@
 //  Copyright © 2019 William Henry. All rights reserved.
 //
 
+//
+//  ViewController.m
+//  ios-13-table-header-bg-color
+//
+//  Created by Tester on 05.07.19.
+//  Copyright © 2019 Tester. All rights reserved.
+//
+
 #import "ViewController.h"
 
 @interface ViewController ()
+@property(strong, nonatomic) NSArray *tableData;
 
 @end
 
@@ -16,7 +25,46 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    self.tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
     // Do any additional setup after loading the view.
+}
+
+#pragma mark - TableView Delegates
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.tableData count];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (nullable NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return @"Foods / Drinks";
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [self.tableData objectAtIndex:indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section{
+    UITableViewHeaderFooterView *header = (UITableViewHeaderFooterView *)view;
+    header.backgroundView.backgroundColor = [UIColor blueColor];
+    [header.textLabel setTextColor:[UIColor whiteColor]];
+    header.textLabel.font = [UIFont systemFontOfSize:22.0 weight:UIFontWeightThin];
 }
 
 
